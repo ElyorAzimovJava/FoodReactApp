4ce830841b40195f956296fb321dc8dfb1c0150b
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-08-28 06:39:51 +0500

    Getting recipes is written

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,18 @@
-import {useState} from "react";
-
+import {use, useEffect, useState} from "react";
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "db98f8b2309b49e493d652ad47c76f43";
 export default function Search(){
     const [query, setQuery] = useState("pizza");
+
+    useEffect(() => {
+       async function fetchFood(){
+          const res= await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`)
+            const data = await res.json();
+            console.log(data.results);
+        }
+        fetchFood();
+        }
+    ,[query])
     return(
         <div>
             <input  onChange={(e) => setQuery(e.target.value)} value={query} type="text" placeholder="Search" />
